142007a6744c5c913dd37693f80cbaca217a3d45
lee-zion <leezion.git@gmail.com>
2022-10-21 16:55:41 +0900

    SOLVED
    REASON OF FAIL
    (100+1+ | 01)+ has a element with length < 6
    OR (|) exists, so a valid minimum valid elements like 01 and 1001 exists
    length limit lower than 6 made me FAIL

--- acmipc/Contact/main.py
+++ acmipc/Contact/main.py
@@ -3,24 +3,15 @@
 # -> check if string can be parsed to 01 or 100+1+
 import re
 import sys
-def main():
-    num_cases = int(sys.stdin.readline())
-    cases_input = []
-    for i in range(num_cases):
-        cases_input.append(sys.stdin.readline().strip())
-    cases_answer = [0]*num_cases
-
-    for i, case_input in enumerate(cases_input):
-        if len(case_input) < 6:
-            cases_answer[i] = 'NO'
-            continue
-        pattern='(100+1+|01)+'
-        if re.fullmatch(pattern, case_input):
-            cases_answer[i] = 'YES'
-        else:
-            cases_answer[i] = 'NO'
-    print(cases_answer)
-    return
-
-if __name__ == '__main__':
-    main()
+N = int(sys.stdin.readline())
+ci = []
+for _ in range(N):
+    ci.append(sys.stdin.readline().strip())
+for c in ci:
+    if len(c) < 2:
+        print('NO')
+        continue
+    if re.fullmatch('(100+1+|01)+', c):
+        print('YES')
+    else:
+        print('NO')
