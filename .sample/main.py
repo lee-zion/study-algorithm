34c0e54b97258c4d215c3e8c9f29481f84835989
lee-zion <leezion.git@gmail.com>
2022-10-22 11:23:35 +0900

    update sample files

--- .sample/main.py
+++ .sample/main.py
@@ -1,14 +1,8 @@
-def readFile(filename):
-    file = open(filename, 'r')
-    ret = file.readlines()
-    for line in range(len(ret)):
-        ret[line] = ret[line].strip()
-    file.close()
-    return ret
-    
-def main(*args, **kwargs):
-    file_raw = readFile('input.txt')
-    print(file_raw)
+import sys
+
+def main():
+    N = sys.stdin.readline()
+    print(N)
 
 if __name__ == "__main__":
     main()
