d63a74ab4dc2d8dd63606ec254858cd8f561a511
lee-zion <leezion.git@gmail.com>
2022-10-29 11:16:01 +0900

    SOVLED
    To make functions as general as possible, main code is messy.
    It considers both adjacent matrix and list, and starting index, too.
    
    To make things clear in submit, created additional file to clean up.
    
    However, output.txt is not utilized in testing.
    To make automatic testing to work, need more work now.

--- DFS와 BFS/test.py
+++ DFS와 BFS/test.py
@@ -1,5 +1,30 @@
-import sys
+from operator import is_
+import unittest
 from collections import defaultdict, deque
+
+def read_file(filename):
+    file = open(filename, 'r')
+    ret = file.readlines()
+    for i, l in enumerate(ret):
+        ret[i] = l.strip()
+    file.close()
+    return ret
+
+def get_adj_matrix(N, g_raw, FROM_INDEX_ZERO = True):
+    graph = None
+    if FROM_INDEX_ZERO:
+        graph = [[0 for _ in range(N)] for __ in range(N)]
+    else:
+        graph = [[0 for _ in range(N+1)] for __ in range(N+1)]
+    for g in g_raw:
+        [depart, dest] = map(int, g.split(" "))
+        if FROM_INDEX_ZERO:
+            graph[depart-1][dest-1] = 1
+            graph[dest-1][depart-1] = 1
+        else:
+            graph[dest][depart] = 1
+            graph[depart][dest] = 1
+    return graph
 
 def get_adj_list(g_raw, FROM_INDEX_ZERO = True, IS_BIDIR = True):
     graph = defaultdict(list)
@@ -17,12 +42,11 @@ def get_adj_list(g_raw, FROM_INDEX_ZERO = True, IS_BIDIR = True):
         graph[g].sort()
     return graph
 
-def main():
-    [N, M, V] = map(int, sys.stdin.readline().split(" "))
-    graph = []
-    for i in range(M):
-        graph.append(sys.stdin.readline().strip())
-    graph = get_adj_list(graph)
+def main(N, M, V, graph_raw):
+    graph_mat = get_adj_matrix(N, graph_raw)
+    graph_list = get_adj_list(graph_raw)
+    # print(f"graph_mat : {graph_mat}")
+    # print(f"graph_list: {graph_list}")
 
     def dfs(current, graph, visited, FROM_INDEX_ZERO = True):
         nonlocal answer
@@ -75,13 +99,42 @@ def main():
                         else:
                             answer.append(adj)
     
+    print(f"---------- [list] ----------")
+    print(f"---------- [DFS] ----------")
     answer, visited = [], [False] * N
-    dfs(V-1, graph, visited)
-    print(" ".join(map(str, answer)))
+    dfs(V-1, graph_list, visited)
+    print(answer)
+
+    print(f"---------- [BFS] ----------")
+
     answer, visited = [], [False] * N
-    bfs(V-1, graph, visited)
-    print(" ".join(map(str, answer)))
+    bfs(V-1, graph_list, visited)
+    print(answer)
+
+    print(f"---------- [matrix] ----------")
+    # ISSUE: CASE3 is not solved in matrix only
+    print(f"---------- [DFS] ----------")
+
+    answer, visited = [], [False] * N
+    dfs(V-1, graph_mat, visited)
+    print(answer)
+    
+    print(f"---------- [BFS] ----------")
+    answer, visited = [], [False] * N
+    bfs(V-1, graph_mat, visited)
+    print(answer)
+
+    return True
+
+class TestCases(unittest.TestCase):
+    def test_input_txt(self):
+        ex1_input = read_file('DFS와 BFS/input.txt')
+        [N, M, V] = map(int, ex1_input[0].split(" "))
+        graph = ex1_input[1:]
+        ex1_output = read_file('DFS와 BFS/output.txt')
+        main(N, M, V, graph)
+        # self.assertEqual(main(N, M, V, graph), ex1_output)
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    unittest.main()
