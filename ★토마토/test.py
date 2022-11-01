2ad7abe62552575ea00ec95bc48e257f0db29867
lee-zion <leezion.git@gmail.com>
2022-11-01 19:16:24 +0900

    SYNC

--- ★토마토/test.py
+++ ★토마토/test.py
@@ -1,5 +1,7 @@
 import unittest
-
+import pprint
+from collections import deque
+pp = pprint.PrettyPrinter(indent=4)
 def read_file(filename):
     file = open(filename, 'r')
     ret = file.readlines()
@@ -9,12 +11,48 @@ def read_file(filename):
     return ret
 
 def main(input):
+    # down, up, left, right
+    dx, dy = [1, -1, 0, 0], [0, 0, -1, 1]
+    [COL_MAX, ROW_MAX] = map(int, input[0].split(" "))
+    graph = []
+    visited = [ [0]*COL_MAX for _ in range(ROW_MAX)]
+    for row in range(ROW_MAX):
+        graph.append(list(map(int, input[1+row].split(" "))))
+    pp.pprint(graph)
+    # for row in range(M):
+
+    # answer = 0
+    answer = 0
+
+    # is all element 1?
+    already_done = True
+    for row in range(ROW_MAX):
+        if not all(graph[row]):
+            already_done = False
+
+    # Y -> return 0
+    if already_done:
+        return answer
+    
+    # N -> contaminate over iteration
+    q = deque([])
+    for col in range(COL_MAX):
+        for row in range(ROW_MAX):
+            if graph[row][col]:
+                q.append((row, col))
+    # find all 1, contaminate the adjacent
+    # answer += 1
+    while q:
+        (x, y) = q.popleft()
+
+
+
     return True
 
 class TestCases(unittest.TestCase):
     def test_input_txt(self):
-        input = read_file('problem title/input.txt')
-        answer = read_file('problem title/output.txt')
+        input = read_file('★토마토/input.txt')
+        answer = read_file('★토마토/output.txt')
         self.assertEqual(main(input), answer)
 
 
