67de8a61956a0dc30a4dfbd7ee5fe0c74f139512
lee-zion <leezion.git@gmail.com>
2022-11-06 22:29:06 +0900

    NEW: CLASS 3 Problems

--- ★AC/test.py
+++ ★AC/test.py
@@ -13,8 +13,8 @@ def main(input):
 
 class TestCases(unittest.TestCase):
     def test_input_txt(self):
-        input = read_file('problem title/input.txt')
-        answer = read_file('problem title/output.txt')
+        input = read_file('★AC/input.txt')
+        answer = read_file('★AC/output.txt')
         self.assertEqual(main(input), answer)
 
 
