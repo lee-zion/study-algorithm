e627617188cce3b60640b0b06607dca7b12d6d69
lee-zion <leezion.git@gmail.com>
2022-11-07 10:50:15 +0900

    SOLVED
    Reverse가 들어와도 실제로 list.reverse()를 실행하지 않음.
    대신, deque.pop()과 deque.popleft()를 reverse 상태에 따라 선택적으로 사용.

--- ★AC/main.py
+++ ★AC/main.py
@@ -1,8 +1,40 @@
 import sys
+from collections import deque
 
 def main():
-    N = sys.stdin.readline()
-    print(N)
+    n_test = int(sys.stdin.readline())
+    offset = 0
+    answer = []
+    input = [n_test]
+    for _ in range(n_test*3):
+        input.append(sys.stdin.readline().strip())
+    for i in range(1, n_test + 1):
+        try:
+            commands = input[offset + 1]
+            merged = input[offset + 3].strip("[").strip("]")
+            arr = []
+            if merged != "":
+                arr = list(map(int, merged.split(",")))
+            offset += 3
+            q = deque(arr)
+            is_reversed = False
+            for cmd in commands:
+                if cmd == "R":
+                    is_reversed = not is_reversed
+                elif cmd == "D":
+                    if is_reversed:
+                        q.pop()
+                    else:
+                        q.popleft()
+            arr = list(q)
+            if is_reversed:
+                arr.reverse()
+            ret = "[" + ",".join(map(str, arr)) + "]"
+            answer.append(str(ret))
+        except IndexError:
+            answer.append("error")
+    for i in answer:
+        print(i)
 
 if __name__ == "__main__":
     main()
--- ★AC/test.py
+++ ★AC/test.py
@@ -1,4 +1,5 @@
 import unittest
+from collections import deque
 
 def read_file(filename):
     file = open(filename, 'r')
@@ -9,7 +10,36 @@ def read_file(filename):
     return ret
 
 def main(input):
-    return True
+    n_test = int(input[0]) + 1
+    offset = 0
+    answer = []
+    for i in range(1, n_test):
+        commands = input[offset + 1]
+        merged = input[offset + 3].strip("[").strip("]")
+        arr = []
+        if merged != "":
+            arr = list(map(int, merged.split(",")))
+        offset += 3
+        q = deque(arr)
+        is_reversed = False
+        for cmd in commands:
+            if cmd == "R":
+                is_reversed = not is_reversed
+            elif cmd == "D":
+                if not q:
+                    answer.append("error")
+                elif is_reversed:
+                    q.pop()
+                else:
+                    q.popleft()
+        arr = list(q)
+        if is_reversed:
+            arr.reverse()
+        # ret = "[" + ",".join(map(str, arr)) + "]"
+        ret = f'[{",".join(map(str, arr))}]'
+        # ret = f'[{",".join(list(q))}]'
+        answer.append(str(ret))
+    return answer
 
 class TestCases(unittest.TestCase):
     def test_input_txt(self):
