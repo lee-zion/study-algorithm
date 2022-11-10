a013e9bebdfac6589e7bcb1e651d3dd1ddc09968
lee-zion <leezion.git@gmail.com>
2022-11-10 15:58:09 +0900

    FAILED
    DFS로 구현하면 시간초과로 해결 불가.

--- ★계단 오르기/main.py
+++ ★계단 오르기/main.py
@@ -1,8 +1,30 @@
 import sys
 
 def main():
-    N = sys.stdin.readline()
-    print(N)
+    n_stair = int(sys.stdin.readline())
+    stairs = []
+    for i in range(n_stair):
+        stairs.append(int(sys.stdin.readline()))
+    def dfs(curr, visited, point):
+        nonlocal stairs, answer
+        if not visited[curr]:
+            visited[curr] = True
+            point += stairs[curr]
+            if curr == len(visited) - 1:
+                answer = max(answer, point)
+            elif curr < len(visited) - 1:
+                if curr + 2 <= len(visited) - 1:
+                    dfs(curr + 2, visited, point)
+                    visited[curr+2] = False
+                if not visited[curr-1]:
+                    dfs(curr + 1, visited, point)
+                    visited[curr+1] = False
+    answer = 0
+    visited = [False]*n_stair
+    dfs(0, visited, 0)
+    visited = [False]*n_stair
+    dfs(1, visited, 0)
+    print(answer)
 
 if __name__ == "__main__":
     main()
--- ★계단 오르기/test.py
+++ ★계단 오르기/test.py
@@ -9,7 +9,41 @@ def read_file(filename):
     return ret
 
 def main(input):
-    return True
+    # 한번에 1 또는 2개 계단만
+    # 연속 3개 계단 금지
+    # 마지막은 반드시 밟기
+    n_stair = int(input[0])
+
+    stairs = []
+    for i in range(n_stair):
+        stairs.append(int(input[1+i]))
+    
+    answer = 0
+    steps = [1, 2]
+    def dfs(curr, visited, point):
+        nonlocal stairs
+        if not visited[curr]:
+            visited[curr] = True
+            point += stairs[curr]
+            nonlocal answer
+            if curr == len(visited) - 1:
+                answer = max(answer, point)
+            elif curr < len(visited) - 1:
+                # range check
+                # curr
+                if curr + 2 <= len(visited) - 1:
+                    dfs(curr + 2, visited, point)
+                    visited[curr+2] = False
+                if not visited[curr-1]:
+                    dfs(curr + 1, visited, point)
+                    visited[curr+1] = False
+    visited = [False]*n_stair
+    dfs(0, visited, 0)
+    visited = [False]*n_stair
+    dfs(1, visited, 0)
+    
+    print(answer)
+    return [str(answer)]
 
 class TestCases(unittest.TestCase):
     def test_input_txt(self):
