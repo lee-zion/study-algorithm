65e81b359687f4c1bb33610bcd84b178f0e02ee3
lee-zion <leezion.git@gmail.com>
2022-11-12 23:33:09 +0900

    SOLVED
    팩토리얼 계산을 재귀함수 대신 for loop로 변경하였음.
    test.py에서 실행시간 측정 결과 시간을 88% 이상 단축시킴.
    실제 제출 metric은 아래와 같이 변화했음.
    
    메모리: 31012kb → 30840kb
    시간: 84ms → 68ms
    코드길이: 367B → 343B

--- 팩토리얼 0의 개수/main.py
+++ 팩토리얼 0의 개수/main.py
@@ -1,8 +1,18 @@
 import sys
 
 def main():
-    N = sys.stdin.readline()
-    print(N)
+    n = int(sys.stdin.readline())
+    n_int = 1
+    for i in range(1, n+1):
+        n_int *= i
+    n_str = str(n_int)
+    target = "0"
+    answer = 0
+    for i in range(len(n_str) - 1, -1, -1):
+        if n_str[i] != target:
+            break
+        answer += 1
+    print(answer)
 
 if __name__ == "__main__":
     main()
--- 팩토리얼 0의 개수/test.py
+++ 팩토리얼 0의 개수/test.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest, time
 
 def read_file(filename):
     file = open(filename, 'r')
@@ -9,14 +9,27 @@ def read_file(filename):
     return ret
 
 def main(input):
-    return True
+    start = time.perf_counter()
+    n = int(input[0])
+    n_int = 1
+    for i in range(1, n+1):
+        n_int *= i
+    n_str = str(n_int)
+    target = "0"
+    answer = 0
+    for i in range(len(n_str) - 1, -1, -1):
+        if n_str[i] != target:
+            break
+        answer += 1
+    end = time.perf_counter()
+    print(f"Time elapsed: {(end - start)*10**3} ms")
+    return [str(answer)]
 
 class TestCases(unittest.TestCase):
     def test_input_txt(self):
         input = read_file('팩토리얼 0의 개수/input.txt')
         answer = read_file('팩토리얼 0의 개수/output.txt')
         self.assertEqual(main(input), answer)
-
 
 if __name__ == '__main__':
     unittest.main()
