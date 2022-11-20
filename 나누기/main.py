211902a0aa4fea60c162cd5fd229b149eaf46409
lee-zion <leezion.git@gmail.com>
2022-11-21 06:09:13 +0900

    FAILED
    시간 초과 오류가 발생함.
    알고리즘 자체는 완성되었으며 입력 갯수가 정해지지 않았을 때 이를 처리하는 방법의 문제로 판단하고 있음.

--- 나누기/main.py
+++ 나누기/main.py
@@ -1,8 +1,19 @@
 import sys
 
 def main():
-    N = sys.stdin.readline()
-    print(N)
-
+    tabs = [list(map(int, line.split())) for line in sys.stdin.readlines()]
+    s = "is not an integer with less than 100 digits."
+    for t, a, b in tabs:
+        u = f"({t}^{a}-1)/({t}^{b}-1)"
+        num = pow(t, a) - 1
+        den = pow(t, b) - 1
+        if num % den:
+            print(f"{u} {s}")
+            continue
+        result = num // den
+        if len(str(result)) < 100:
+            print(f"{u} {result}")
+        else:
+            print(f"{u} {s}")
 if __name__ == "__main__":
     main()
--- 나누기/test.py
+++ 나누기/test.py
@@ -8,13 +8,36 @@ def read_file(filename):
     file.close()
     return ret
 
-def main(*args):
-    return True
+def main(input):
+    tabs = []
+    for i in range(len(input)):
+        tabs.append(list(map(int, input[i].split())))
 
+    s = "is not an integer with less than 100 digits."
+    answer = []
+    for [t, a, b] in tabs:
+        u = f"({t}^{a}-1)/({t}^{b}-1)"
+        print(t, a, b)
+        num = pow(t, a) - 1
+        den = pow(t, b) - 1
+        # result is not integer
+        if num % den:
+            print(f"{u} {s}")
+            answer.append(f"{u} {s}")
+            continue
+        result = num // den
+        if len(str(result)) < 100:
+            print(f"{u} {result}")
+            answer.append(f"{u} {result}")
+        else:
+            print(f"{u} {s}")
+            answer.append(f"{u} {s}")
+    return answer
 class TestCases(unittest.TestCase):
     def test_input_txt(self):
-        answer = read_file('<path_to_file>')
-        self.assertEqual(main(), answer)
+        input = read_file('나누기/input.txt')
+        answer = read_file('나누기/output.txt')
+        self.assertEqual(main(input), answer)
 
 
 if __name__ == '__main__':
