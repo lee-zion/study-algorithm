e7377ca83fc41d927458a101cdd71597586b45fc
lee-zion <leezion.git@gmail.com>
2022-11-21 07:22:14 +0900

    SYNC: from home

--- 리모컨/test.py
+++ 리모컨/test.py
@@ -1,22 +1,62 @@
 import unittest
 
+
 def read_file(filename):
-    file = open(filename, 'r')
+    file = open(filename, "r")
     ret = file.readlines()
     for i, l in enumerate(ret):
         ret[i] = l.strip()
     file.close()
     return ret
 
+
 def main(input):
+    begin = 100
+    target = int(input[0])
+    n_broken = int(input[1])
+    brokens = list(map(int, input[2].split(" ")))
+    candidates = list(range(10))
+    for i, broken in enumerate(brokens):
+        del candidates[broken - i]
+    
+    answer = 5*10**5
+    channel = ""
+    def dfs(channel):
+        nonlocal answer, target, candidates
+        for btn in candidates:
+            channel += str(btn)
+            answer = min(answer, abs(int(channel) - target) + len(channel))
+            if len(str(channel)) < len(str(target)) + 1:
+                dfs(channel)
+            else:
+                channel = channel[:-1]
+    dfs(channel)
+
+
+    answer = min(answer + abs(answer - target), abs(answer - begin))
+    # for power in range(len(target)):
+    #     digit = target[-power-1]
+    # # for power, digit in enumerate(target):
+    #     dist = []
+    #     digit = int(digit)
+    #     for cand in candidates:
+    #         if cand > digit:
+    #             dist.append(cand - digit)
+    #         else:
+    #             dist.append(digit - cand)
+    #     # find the index of minimum value
+    #     val, idx = min((val, idx) for (idx, val) in enumerate(dist))
+    #     answer += candidates[idx] * 10**power
+
     return True
+
 
 class TestCases(unittest.TestCase):
     def test_input_txt(self):
-        input = read_file('리모컨/input.txt')
-        answer = read_file('리모컨/output.txt')
+        input = read_file("리모컨/input.txt")
+        answer = read_file("리모컨/output.txt")
         self.assertEqual(main(input), answer)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
--- 물병/test.py
+++ 물병/test.py
@@ -8,13 +8,40 @@ def read_file(filename):
     file.close()
     return ret
 
-def main(*args):
+def main(input):
+    # capability : maximum continuous 1's
+    # cnt        : current continuous 1's
+    # if current <= capability
+    bottle, capability = map(int, input[0].split())
+    bottle_bin = bin(bottle)[2:]
+    # get desired minimum
+    desired = ""
+    for i in range(capability):
+        desired += "1"
+    
+    answer = []
+    # count continuous 1's from bottle_bin
+    cnt, i_cnt = 0, 0
+    for i, b in enumerate(bottle_bin):
+        if b == '1':
+            cnt += 1
+        else:
+            i_cnt = i
+            break
+    no_ones = True
+    for i in bottle_bin[i_cnt:]:
+        if i == "1":
+            no_ones = False
+            break
+    # ending condition
+    # if cnt <= capability and no_ones:
     return True
 
 class TestCases(unittest.TestCase):
     def test_input_txt(self):
-        answer = read_file('<path_to_file>')
-        self.assertEqual(main(), answer)
+        input = read_file('물병/input1.txt')
+        answer = read_file('물병/output1.txt')
+        self.assertEqual(main(input), answer)
 
 
 if __name__ == '__main__':
