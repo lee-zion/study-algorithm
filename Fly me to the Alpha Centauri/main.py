5b0b5bf7bf73f461bb220481113ab2ee2424298f
lee-zion <leezion.git@gmail.com>
2022-11-25 18:10:47 +0900

    log2 vs isqrt...

--- Fly me to the Alpha Centauri/main.py
+++ Fly me to the Alpha Centauri/main.py
@@ -1,8 +1,20 @@
 import sys
+from math import log2, pow
 
 def main():
-    N = sys.stdin.readline()
-    print(N)
+    answer = []
+    T = int(sys.stdin.readline().strip())
+    for i in range(T):
+        depart, dest = map(int, sys.stdin.readline().strip().split())
+        diff = dest - depart
+        n = int(log2(diff))
+        n2 = pow(n, 2)
+        if diff == n2:
+            print(2*n - 1)
+        elif diff > n2 + n:
+            print(2*n + 1)
+        else:
+            print(2*n)
 
 if __name__ == "__main__":
     main()
