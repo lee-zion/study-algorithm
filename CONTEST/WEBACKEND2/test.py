d618860663a80801f0e4baaa55a51237aa1bc049
lee-zion <leezion.git@gmail.com>
2022-11-30 00:32:14 +0900

    RENAME: 실전 코딩테스트 문제들을 CONTEST로 이동
    LG CNS Code Monster, 프로그래머스 웹 백엔드 2022 마지막

---
+++ CONTEST/WEBACKEND2/test.py
@@ -0,0 +1,39 @@
+import unittest
+from traceback import print_exception
+import sys
+
+def read_file(filename):
+    file = open(filename, 'r')
+    ret = file.readlines()
+    for i, l in enumerate(ret):
+        ret[i] = l.strip()
+    file.close()
+    return ret
+
+def main(inputs):
+    answers = []
+    try:
+        for input in inputs:
+            # your code here
+            answer = True
+            answers.append(answer)
+        return answers
+    except Exception:
+        print(f"===========================================================================")
+        print(f"Failed in the case below")
+        print(f"input: {input}")
+        exc_info = sys.exc_info()
+        print_exception(*exc_info)
+        print(f"===========================================================================")
+        del exc_info
+class TestCases(unittest.TestCase):
+    def test_input_txt(self):
+        inputs, answers = [], []
+        for i in range(1, 1 + 1):
+            inputs.append(read_file(f"WEBACKEND2/input{i}.txt"))
+            answers.append(read_file(f"WEBACKEND2/output{i}.txt")[0])
+        self.assertEqual(main(inputs), answers)
+
+
+if __name__ == '__main__':
+    unittest.main()
