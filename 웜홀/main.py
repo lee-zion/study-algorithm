74a7f2410dbbf5fdf9de7603634fa0e6cd706b42
lee-zion <leezion.git@gmail.com>
2022-12-05 01:36:00 +0900

    FAILED: 웜홀
    현재 DFS 완전탐색으로도 상상할 수 있는 경우의 수들을 처리하고 있는 것 같음.
    그런데 무슨 반례가 있어서 그런지 11% 시작부터 틀림.
    문제 질문을 보면 DFS/BFS 외의 그래프 탐색 알고리즘들을 알아야 할 것 같음.
    다른 문제에서도 계속 등장했으니, 이제는 유명한 탐색 알고리즘들은 정리해야 함.
    트리/그래프 자료구조 정의 및 탐색 알고리즘을 어딘가에 한 번 정리하자.

--- 웜홀/main.py
+++ 웜홀/main.py
@@ -1,8 +1,53 @@
-import sys
+from sys import stdin
 
 def main():
-    N = sys.stdin.readline()
-    print(N)
+    T = int(stdin.readline())
+    GRAPH_INIT = 10**4 + 1
+    for i in range(T):
+        n, m, w = map(int, stdin.readline().split())
+
+        roads = []
+        for _ in range(1, m + 1):
+            roads.append(list(map(int, stdin.readline().split())))
+        holes = []
+        for _ in range(1, w + 1):
+            holes.append(list(map(int, stdin.readline().split())))
+        graph = [[GRAPH_INIT] * (n) for _ in range(n)]
+        for s, e, t in roads:
+            graph[s - 1][e - 1] = min(graph[s - 1][e - 1], t)
+            graph[e - 1][s - 1] = min(graph[e - 1][s - 1], t)
+        for s, e, t in holes:
+            graph[s - 1][e - 1] = -t
+
+        for i in range(n):
+            for j in range(n):
+                if graph[i][j] == GRAPH_INIT:
+                    graph[i][j] = 0
+
+        def dfs(time, curr, graph, visited):
+            nonlocal answer, begin
+            if curr == begin:
+                answer = min(answer, time)
+            else:
+                for col, adj in enumerate(graph[curr]):
+                    if not visited[col] and adj != 0:
+                        visited[col] = True
+                        dfs(time + adj, col, graph, visited)
+                        visited[col] = False
+
+        answer = 0
+        for begin in range(n):
+            answer = GRAPH_INIT
+            visited = [False] * (n + 1)
+            for row, adj in enumerate(graph[begin]):
+                dfs(adj, row, graph, visited)
+                if answer < 0:
+                    answer = "YES"
+                    break
+            if answer != "YES":
+                answer = "NO"
+        print(answer)
+
 
 if __name__ == "__main__":
     main()
--- 웜홀/test.py
+++ 웜홀/test.py
@@ -2,38 +2,114 @@ import unittest
 from traceback import print_exception
 import sys
 
+
 def read_file(filename):
-    file = open(filename, 'r')
+    file = open(filename, "r")
     ret = file.readlines()
     for i, l in enumerate(ret):
         ret[i] = l.strip()
     file.close()
     return ret
 
+
 def main(inputs):
     answers = []
     try:
+        GRAPH_INIT = 10**4 + 1
         for input in inputs:
             # your code here
-            answer = True
-            answers.append(answer)
+            T = int(input[0])
+            offset = 0
+            for i in range(T):
+                # n: number of node, [1, 500]
+                # m: number of road, [1, 2500]
+                # w: number of wormhole, [1, 200]
+                n, m, w = map(int, input[1 + i + offset].split())
+                
+                roads = []
+                for i_road in range(1, m + 1):
+                    # s, e, t
+                    # s,e: node s and e are connected to each other
+                    # t: time needed, t \in [0, 10**4]
+                    roads.append(
+                        list(map(int, input[1 + i + offset + i_road].split()))
+                    )
+                holes = []
+                for i_hole in range(1, w + 1):
+                    # wormhole: one-way road
+                    # s, e, t
+                    # s: starting
+                    # e: ending
+                    # t: time decreased, t \in [0, 10**4]
+                    holes.append(
+                        list(
+                            map(int, input[1 + i + offset + m + i_hole].split())
+                        )
+                    )
+                graph = [ [GRAPH_INIT] * (n) for _ in range(n)]
+                for s, e, t in roads:
+                    graph[s-1][e-1] = min(graph[s-1][e-1], t)
+                    graph[e-1][s-1] = min(graph[e-1][s-1], t)
+                for s, e, t in holes:
+                    graph[s-1][e-1] = -t
+                
+                for i in range(n):
+                    for j in range(n):
+                        if graph[i][j] == GRAPH_INIT:
+                            graph[i][j] = 0
+                
+                # 완전탐색?
+                def dfs(time, curr, graph, visited):
+                    nonlocal answer, begin
+                    # answer = min(answer, time)
+                    # 나 자신에게 돌아오는 path 전부 찾기
+                    if curr == begin:
+                        answer = min(answer, time)
+                    else:
+                        for col, adj in enumerate(graph[curr]):
+                            # print(f"col: {col}   adj: {adj}   curr: {curr}")
+                            # print(f"col: {type(col)}   adj: {type(adj)}   curr: {type(curr)}")
+                            if not visited[col] and adj != 0:
+                                visited[col] = True
+                                dfs(time + adj, col, graph, visited)
+                                visited[col] = False
+                answer = 0
+                for begin in range(n):
+                    answer = GRAPH_INIT
+                    visited = [False] * (n+1)
+                    for row, adj in enumerate(graph[begin]):
+                        dfs(adj, row, graph, visited)
+                        if answer < 0:
+                            answer = "YES"
+                            break
+                    if answer != "YES":
+                        answer = "NO"
+
+                offset = offset + m + w
+                answers.append(answer)
         return answers
     except Exception:
-        print(f"===========================================================================")
+        print(
+            f"==========================================================================="
+        )
         print(f"Failed in the case below")
         print(f"input: {input}")
         exc_info = sys.exc_info()
         print_exception(*exc_info)
-        print(f"===========================================================================")
+        print(
+            f"==========================================================================="
+        )
         del exc_info
+
+
 class TestCases(unittest.TestCase):
     def test_input_txt(self):
         inputs, answers = [], []
-        for i in range(1, 1 + 1):
+        for i in range(3, 3 + 1):
             inputs.append(read_file(f"웜홀/input{i}.txt"))
-            answers.append(int(read_file(f"웜홀/output{i}.txt")[0]))
-        self.assertEqual(main(inputs), answers)
+            answers.append(read_file(f"웜홀/output{i}.txt"))
+        self.assertEqual(main(inputs), answers[0])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
