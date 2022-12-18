c27bd629c987cf19c2a5b5e196e0ce6c0e8ef872
lee-zion <leezion.git@gmail.com>
2022-12-18 16:12:58 +0900

    SOLVING: 최소비용 구하기
    Dijkstra 알고리즘은 구현 완료하였음.
    하지만 edge case에 대한 처리가 미비한 듯 함.
    33%에서 실패하고 있음.
    참고: https://techblog-history-younghunjo1.tistory.com/247

--- 최소비용 구하기/main.py
+++ 최소비용 구하기/main.py
@@ -1,8 +1,38 @@
 import sys
+from collections import deque
 
+input = sys.stdin.readline
 def main():
-    N = sys.stdin.readline()
-    print(N)
+    cities, buses = map(int, (input(), input()))
+    VAL_INF, KEY_INF = pow(10, 6), -1
+    graph = [[VAL_INF]*(cities + 1) for _ in range(cities + 1)]
+    
+    for bus in range(buses):
+        depart, dest, cost = map(int, input().split())
+        graph[depart][dest] = cost
+    
+    depart, dest = map(int,input().split())
+    visited = [False] * (cities + 1)
+    def dijkstra(begin, dest):
+        nonlocal graph, visited, VAL_INF, KEY_INF
+        q = deque([begin])
+        dist = [VAL_INF] * (cities + 1)
+        dist[begin] = 0
+        while q:
+            curr = q.popleft()
+            visited[curr] = True
+            val_min, key_min = VAL_INF, KEY_INF
+            for i_adj, adj in enumerate(graph[curr]):
+                if not visited[i_adj] and adj != VAL_INF:
+                    dist[i_adj] = min(dist[i_adj], dist[curr] + adj)
+                    if val_min > dist[i_adj]:
+                        val_min = dist[i_adj]
+                        key_min = i_adj
+            if key_min != KEY_INF:
+                next = key_min
+                q.append(next)
+        return dist[dest]
+    print(dijkstra(depart, dest))
 
 if __name__ == "__main__":
     main()
--- 최소비용 구하기/test.py
+++ 최소비용 구하기/test.py
@@ -1,6 +1,8 @@
 import unittest
 from traceback import print_exception
 import sys
+from collections import deque
+from heapq import heappop, heappush
 
 def read_file(filename):
     file = open(filename, 'r')
@@ -15,7 +17,44 @@ def main(inputs):
     try:
         for input in inputs:
             # your code here
-            answer = True
+            cities, buses = map(int, (input[0], input[1]))
+            # INF = -1
+            VAL_INF, KEY_INF = pow(10, 10) + 1, -1
+            graph = [[] for _ in range(cities + 1)]
+            # graph = [[VAL_INF]*(cities + 1) for _ in range(cities + 1)]
+            
+            # initialize distance table
+            # for bus in range(buses):
+            #     depart, dest, cost = map(int, input[2+bus].split())
+            #     graph[depart][dest] = min(graph[depart][dest], cost)
+            
+            # set departure/destination node
+            depart, dest = map(int,input[2 + buses].split())
+            visited = [False] * (cities + 1)
+            def dijkstra(begin, dest):
+                nonlocal graph, visited, VAL_INF, KEY_INF
+                q = deque([begin])
+                # distance from begin to i-th index
+                dist = [VAL_INF] * (cities + 1)
+                dist[begin] = 0
+                # for i in range(cities - 1):
+                while q:
+                    curr = q.popleft()
+                    # find unvisited node from adjacent of graph[curr]
+                    visited[curr] = True
+                    val_min, key_min = VAL_INF, KEY_INF
+                    for i_adj, adj in enumerate(graph[curr]):
+                        # adj: distance from curr to i-adj-th index
+                        if not visited[i_adj] and adj != VAL_INF:
+                            dist[i_adj] = min(dist[i_adj], dist[curr] + adj)
+                            if val_min > dist[i_adj]:
+                                val_min = dist[i_adj]
+                                key_min = i_adj
+                    if key_min != KEY_INF:
+                        next = key_min
+                        q.append(next)
+                return dist[dest]
+            answer = dijkstra(depart, dest)
             answers.append(answer)
         return answers
     except Exception:
@@ -29,7 +68,7 @@ def main(inputs):
 class TestCases(unittest.TestCase):
     def test_input_txt(self):
         inputs, answers = [], []
-        for i in range(1, 1 + 1):
+        for i in range(6, 6 + 1):
             inputs.append(read_file(f"최소비용 구하기/input{i}.txt"))
             answers.append(int(read_file(f"최소비용 구하기/output{i}.txt")[0]))
         self.assertEqual(main(inputs), answers)
