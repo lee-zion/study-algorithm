1c7e0fdf0573150d499f70dfa352e6070a33cca0
lee-zion <leezion.git@gmail.com>
2023-01-11 09:05:33 +0900

    NEW: LIS 3/4/5, 반도체 설계, 전깃줄
    전부 LIS에 관련된 문제

---
+++ 전깃줄/main.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+def main():
+    # your code here
+    n = int(input().strip())
+    n = map(int, input().strip().split())
+    n = input().strip().split()
+
+if __name__ == "__main__":
+    main()
